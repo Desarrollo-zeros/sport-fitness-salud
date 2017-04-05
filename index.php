4eba74f1a0e621dd15f9f71fa0cbbebd123dba7e
zeros <wowzeros2@gmail.com>
2017-04-05 15:15:42 -0500

    Sport Fitness Salud

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/Web.css">
+</head>
+<body>
+
+<div class="header">
+  <h1>Sport Fitness Salud</h1>
+</div>
+
+
+<div class="pull-right btn-group">
+    <a href="https://www.facebook.com/walmirjose.medinamedina" class="btn btn-info" role="button">Facebook Desarrollador Negocio</a>
+	<br><br>
+	<a href="https://www.facebook.com/carlos.castilla.79"  class="btn btn-info" role="button">Facebook Desarrollador 1</a></br>
+</div>
+<div class="row">
+
+<div class="col-3 col-s-3 menu">
+  <ul> 
+  <a href="#"><li>Inicio</li></a> 
+  <a href="#Who"><li>¿Quienes Somos?</li></a> 
+  <a href="#what"><li>¿Que Haremos? Video ...</li></a> 
+  <a href="#folder"><li>Archivos Desarrollador De Negocio</li></a> 
+  <a href="#folder1"><li>Archivos Desarrollador 1</li></a> 
+  </ul>
+</div>
+
+<div class="col-6 col-s-9">
+  <h1>Sport Fitness Salud</h1>
+  <hr></hr>
+  <hr></hr>
+  <img src="img/bg.jpg" width="460" height="345">
+  <br>
+    <br>
+  <dt id="Who"><h1>¿Quienes Somos?</h1></dt> 
+  <b><p>Somos un grupo de Emprendedores que buscamos Mejorar la salud de las personas<p></b>
+  <b><p>por medio de la actividad física dirigida esta conformado por: </p></b>
+  <div class="col-6 col-s-9">
+	<p><h3>Desarrollador De Negocio</h3></p>
+	<img src="img/walmir.jpg" width="460" height="345">
+	<p><B>Nombre: walmir jose medina</B><p>	
+	<p><B>Estudio: estudiante ingenieria de sistemas</B><p>
+	<p><B>Rol: Desarrollador de negocio</B><p>
+	<p><B>Correo: warlock871130@gmail.com</B><p>
+	<p><h3>Desarrollador 1</h3></p>
+	<img src="img/carlos.jpg" width="460" height="345">
+	<dt id="Zeros"><p><B>Nombre: Carlos Andres Castilla Garcia</B><p></dt> 
+	<p><B>Estudio: estudiante ingenieria de sistemas</B><p>
+	<p><B>Rol: Desarrollador 1</B><p>
+	<p><B>Correo: candrescastilla@unicesar.edu.co</B><p>
+	<br>
+	<h1>¿Que Haremos?</h1>
+	<iframe width="800" height="400"
+	src="https://www.youtube.com/embed/XGSy3_Czz8k">
+	</iframe>
+	<dt id="what"></dt> 
+</div>
+</div>
+</div>
+
+<div class="footer">
+  <p>Sport Fitness  salud</p>
+  <p>@Derechos Reservados <a href="#Zeros">Dev Zeros</a></p>
+</div>
+
+</body>
+</html>
